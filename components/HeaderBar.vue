1903b277011a02e1ca645154cb5f3d68241a1483
della3a <manelky@gmail.com>
2023-04-21 14:51:46 +0200

    AVTR-18: button back to start page for the header

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-row items-center p-4 space-x-5">
-    <div class="flex flex-col space-y-1">
+    <button @click="camStore.initializeMediaUI()">
+      <div class="flex flex-col space-y-1">
       <div class="logo-title">Empowered By</div>
       <img src="../static/1024.png" alt="logo" class="w-20 h-10">
     </div>
+    </button>
     <label class="relative cursor-pointer">
     <input type="checkbox" value="" class="sr-only peer">
       <div class="w-11 h-6 bg-gray-600
@@ -26,6 +28,22 @@
   </div>
 </template>
 
+<script>
+import { useCameraStore } from '~~/store'
+
+export default {
+  name: 'HeaderBar',
+  setup () {
+    const camStore = useCameraStore()
+
+    return {
+      camStore
+    }
+  }
+}
+</script>
+
+
 <style>
   .peer::after {
     content: "";
@@ -33,3 +51,4 @@
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z' style='fill: white;'%3E%3C/path%3E%3C/svg%3E");
   }
 </style>
+
