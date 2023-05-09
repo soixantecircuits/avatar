2c2bbb5f3f68e902befb9d8a68b9303b9ebeb45d
della3a <manelky@gmail.com>
2023-05-09 17:10:44 +0200

    AVTR-25: Integrate the Soixante circuits slash light red

--- use/useShader.js
+++ use/useShader.js
@@ -7,7 +7,9 @@ import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-import TextureChess from 'assets/textures/chess.jpg'
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js'
+
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 import { cvsContainer } from '../use/useMedia.js'
 
@@ -22,8 +24,6 @@ let videoSprite = null
 let videoMaterial = null
 let tex = null
 
-let cube = null
-
 let raf = null
 
 let bloomComposer = null
@@ -34,6 +34,8 @@ let finalComposer = null
 let darkMaterial = null
 
 let isMouseDown = false
+
+let group = null
 
 function init (video) {
   // render
@@ -69,9 +71,12 @@ function init (video) {
 
   // scene
   scene = new THREE.Scene()
-  // cameraShader = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 0.1, 1000)
+
   cameraShader = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1000)
   scene.add(cameraShader)
+
+  // const controls = new OrbitControls(cameraShader, renderer.domElement)
+  // controls.screenSpacePanning = true
 
   // the video texture
   tex = new THREE.VideoTexture(video)
@@ -149,22 +154,46 @@ function init (video) {
 
   darkMaterial = new THREE.MeshBasicMaterial({ color: 'black' })
 
-  const geometryCube = new THREE.BoxGeometry(0.1, 0.1, 0.1)
-  const texture = new THREE.TextureLoader().load(TextureChess)
-  const materialCube = new THREE.MeshBasicMaterial({ map: texture })
-  // const materialCube = new THREE.MeshStandardMaterial({
-  //   color: 0xffff00,
-  //   roughness: 0.4,
-  //   transparent: true
-  // })
-  cube = new THREE.Mesh(geometryCube, materialCube)
-  scene.add(cube)
-  cube.position.z = -1
-  cube.position.x = -0.1
-  cube.position.y = -0.1
-
   cameraShader.position.z = 1
 
+  const svgSlash = ` <svg width="96" height="172" viewBox="0 0 96 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g style="mix-blend-mode:difference">
+  <path d="M48.0756 67.0187H38.6574V85.9355H29.2391V104.852H38.6574V85.9355H48.0756V67.0187H57.4939V48.102H48.0756V67.0187ZM19.8209 133.227H10.4026V152.144H0.984375V171.061H10.4026V152.144H19.8209V133.227H29.2391V114.311H19.8209V133.227Z" fill="white"/>
+  <path d="M76.5444 47.9325H67.1261V66.8493H57.7079V85.766H67.1261V66.8493H76.5444V47.9325H85.9626V29.0158H76.5444V47.9325ZM48.2896 114.141H38.8714V133.058H29.4531V151.975H38.8714V133.058H48.2896V114.141H57.7079V95.2244H48.2896V114.141Z" fill="white"/>
+  </g>
+  </svg>
+  `
+  const svgSlashURI = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgSlash)
+
+  const loader = new SVGLoader()
+  loader.load(svgSlashURI, function (data) {
+    const paths = data.paths
+    group = new THREE.Group()
+    group.scale.multiplyScalar(0.001)
+    group.position.x = -0.1
+    group.position.y = -0.1
+    group.position.z = -1
+    // group.scale.y *= -1
+
+    const colorRed = new THREE.Color(0xFF6443)
+
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i]
+      const material = new THREE.MeshBasicMaterial({
+        color: colorRed,
+        side: THREE.DoubleSide,
+        depthWrite: true
+      })
+      const shapes = SVGLoader.createShapes(path)
+      for (let j = 0; j < shapes.length; j++) {
+        const shape = shapes[j]
+        const geometry = new THREE.ShapeGeometry(shape)
+        const mesh = new THREE.Mesh(geometry, material)
+        group.add(mesh)
+      }
+      scene.add(group)
+    }
+  })
   window.addEventListener('mousedown', onMouseDown)
   window.addEventListener('mouseup', onMouseUp)
   window.addEventListener('mousemove', onMouseMove)
@@ -189,22 +218,21 @@ function onMouseMove (event) {
     const x = ((mouseX / width) * 2 - 1) * -1
     const y = -(mouseY / height) * 2 + 1
 
-    const vector = new THREE.Vector3(x, y, 0.5)
+    const vector = new THREE.Vector3(x, y, 0.01)
     vector.unproject(cameraShader)
 
     const dir = vector.sub(cameraShader.position).normalize()
     const distance = -cameraShader.position.z / dir.z
     const pos = cameraShader.position.clone().add(dir.multiplyScalar(distance))
-
-    cube.scale.set(0.5, 0.5, 0.5)
-    cube.position.copy(pos)
+    group.scale.set(0.0005, 0.0005, 0.0005)
+    group.position.copy(pos)
   }
 }
 
 async function animate () {
   raf = requestAnimationFrame(animate)
-  cube.rotation.x += 0.05
-  cube.rotation.y += 0.05
+  // group.rotation.x += 0.05
+  // group.rotation.y += 0.05
 
   // renderer.render(scene, cameraShader)
   videoSprite.material = darkMaterial
