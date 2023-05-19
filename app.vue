b50a9c38abb1e8a1d517c29981a33da2bbc08559
della3a <manelky@gmail.com>
2023-05-19 17:30:54 +0200

    AVTR-35: add more interactivity

--- app.vue
+++ app.vue
@@ -1,18 +1,29 @@
 <template>
-  <div>
+  <div class="relative overflow-hidden">
     <div :class="getBackgroundClasses()" class="relative text-violet-700" >
       <HeaderBar class="absolute top-0" />
       <NuxtPage class="h-screen w-screen flex flex-col justify-center" />
     </div>
+    <div class=" absolute bg-violet bottom-0 rounded-full blur-3xl z-i50"
+      :style="{
+        left: `${x}px`,
+        top: `${y}px`,
+        width: `200px`,
+        height: `200px`,
+      }">
+      </div>
   </div>
 </template>
 
 <script>
 import { useCameraStore } from './store'
+import { useMouse } from '@vueuse/core'
+
 export default {
   name: 'App',
   setup () {
     const camStore = useCameraStore()
+    const { x, y } = useMouse()
 
     const getBackgroundClasses = () => {
       if (camStore.darkMode === true) {
@@ -23,7 +34,10 @@ export default {
     }
     return {
       camStore,
-      getBackgroundClasses
+      getBackgroundClasses,
+      x,
+      y
+
     }
   }
 }
