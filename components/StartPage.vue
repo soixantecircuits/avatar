7214ed2c6fd9cb9c62f402cc81098814fc00aa07
della3a <manelky@gmail.com>
2023-05-30 18:23:01 +0200

    AVTR-55: photo gallery improved

--- components/StartPage.vue
+++ components/StartPage.vue
@@ -5,10 +5,10 @@
       </button>
     <div class="absolute">
       <img class="rounded-lg transform -translate-x-1/2 translate-y-3/4 z-10 w-60 h-60" src="/assets/image1.png" alt="Picture 1" />
-      <img class= "rounded-lg transform translate-x-80 translate-y-40 z-20 w-60 h-60" src="/assets/image2.png" alt="Picture 2" />
+      <img class= "rounded-lg transform translate-x-96 translate-y-40 z-20 w-60 h-60" src="/assets/image2.png" alt="Picture 2" />
       <img class="rounded-lg transform translate-x-60 translate-y-60 z-30 w-60 h-60" src="/assets/image3.png" alt="Picture 3" />
-      <img class="rounded-lg transform -translate-x-60 -translate-y-50  z-5 w-60 h-60" src="/assets/image1.png" alt="Picture 4" />
-      <img class="rounded-lg transform -translate-x-80 -translate-y-1/4 z-15 w-60 h-60" src="/assets/image2.png" alt="Picture 5" />
+      <img class="rounded-lg transform -translate-x-80 -translate-y-40  z-5 w-60 h-60" src="/assets/image1.png" alt="Picture 4" />
+      <img class="rounded-lg transform -translate-x-96 -translate-y-1/4 z-15 w-60 h-60" src="/assets/image2.png" alt="Picture 5" />
       <img class="rounded-lg transform -translate-x-1/6 -translate-y-40 z-25 w-60 h-60" src="/assets/image3.png" alt="Picture 6" />
       <img class="rounded-lg transform -translate-x-1/7 -translate-y-1/8 z-10 w-60 h-60" src="/assets/image1.png" alt="Picture 7" />
     </div>
