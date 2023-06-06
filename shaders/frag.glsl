eb384c0bc990aa4a2e9f892e5615fd506a00edef
della3a <manelky@gmail.com>
2023-06-06 11:10:32 +0200

    AVTR-61: landing page photos in the right size

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -13,43 +13,6 @@ void main() {
   // vec4 texColor = texture(tex, vUv.xy);
   // gl_FragColor = texColor;
 
-  // wave deformation effect
-  // vec2 uv = vUv.xy;
-  // uv.x += sin(uv.y * 40. + time) * 0.01;
-  // uv.y += sin(uv.x * 50. + time) * 0.01;
-  // vec4 texColor = texture(tex, uv);
-  // gl_FragColor = texColor;
-
-  // negative effect
-
-  // vec4 texColor = texture(tex, vUv.xy);
-
-  // if (vUv.x < 0.5) {
-  //   gl_FragColor = vec4(texColor.r, texColor.g, texColor.b, texColor.a);
-  // } else {
-  //   // apply negative effect to the half of the screen
-  //   gl_FragColor = vec4(1. - texColor.r, 1. - texColor.g, 1. - texColor.b, texColor.a);
-  // }
-
-  // rainbow effect with waves
-  // vec2 uv = vUv.xy;
-  // uv.x += sin(uv.y * 40. + time) * 0.01;
-  // uv.y += sin(uv.x * 50. + time) * 0.01;
-  // vec4 texColor = texture(tex, uv);
-
-  // float r = texColor.r;
-  // float g = texColor.g;
-  // float b = texColor.b;
-
-  // if (vUv.x < 0.33) {
-  //   gl_FragColor = vec4(r, g, b, texColor.a);
-  // } else if (vUv.x < 0.66) {
-  //   gl_FragColor = vec4(g, b, r, texColor.a);
-  // } else {
-  //   gl_FragColor = vec4(b, r, g, texColor.a);
-  // }
-
-  // add a dark filter to the image
   vec4 texColor = texture(tex, vUv.xy);
-  gl_FragColor = vec4(texColor.r * 0.5, texColor.g * 0.5, texColor.b * 0.5, texColor.a);
+  gl_FragColor = vec4(texColor.r, texColor.g * 0.8, texColor.b * 0.8, texColor.a);
 }
