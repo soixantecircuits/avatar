a1110d022c7889e1c5f7a5cc6635d34062545004
della3a <manelky@gmail.com>
2023-06-07 11:11:18 +0200

    AVTR-64: Mobile When Dragging the // symbol, the entire page scrolls

--- components/VerifPhotoShoot.vue
+++ components/VerifPhotoShoot.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class='h-screen layer-text'>
-      <img :src='img' class='image absolute bottom-0 w-full height-85 bg-[#0C0C0C] flex flex-col justify-center items-center layer-text' />
+    <div class='absolute top-40 w-full h-screen layer-text'>
+      <img :src='img' class='image w-full height-85 bg-[#0C0C0C] flex flex-col justify-center items-center layer-text' />
       </div>
       <div class='layer-text w-full h-1/5 space-y-5 absolute bottom-0 flex flex-col justify-center items-center filter-bar py-30'>
         <button class='flex flex-col justify-center items-center' @click='camStore.goToShare()'>
