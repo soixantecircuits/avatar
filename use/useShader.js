856570b70f06dff305f6cc273115650ea7865734
della3a <manelky@gmail.com>
2023-06-07 12:11:14 +0200

    AVTR-64: scale the slash

--- use/useShader.js
+++ use/useShader.js
@@ -178,7 +178,7 @@ function init (video) {
     // groupSlash.scale.y *= -1
     // streatch vertically the slash on mobile
     if (window.innerWidth < 768) {
-      groupSlash.scale.x *= 2.5
+      groupSlash.scale.set(0.0025, 0.0008, 0.0008)
     }
 
     groupSlash.renderOrder = 1
@@ -298,11 +298,8 @@ function onTouchEvent (event) {
   pos.x = Math.max(minX, Math.min(maxX, pos.x))
   pos.y = Math.max(minY, Math.min(maxY, pos.y))
 
-  groupSlash.scale.set(0.0004, 0.0004, 0.0004)
+  groupSlash.scale.set(0.005, 0.0004, 0.0004)
   groupSlash.position.copy(pos)
-  if (window.innerWidth < 768) {
-    groupSlash.scale.x *= 2.5
-  }
 }
 
 async function animate () {
