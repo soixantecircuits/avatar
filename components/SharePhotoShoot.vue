0ec98d40fcb11c1971f308495fd69edd3289421e
della3a <manelky@gmail.com>
2023-06-08 15:01:10 +0200

    AVTR-TEST: fix image video size

--- components/SharePhotoShoot.vue
+++ components/SharePhotoShoot.vue
@@ -3,7 +3,7 @@
     <div class='h-full w-full flex flex-row justify-center space-x-7'>
     <div class='height-70 w-8/12 flex flex-col space-y-7'>
       <div class='relative lg:height-60 height-55 w-full flex flex-col'>
-        <img :src='img' class='h-full w-full image rounded-lg flex lg:object-none lg:object-left object-center object-cover' />
+        <img :src='img' class='h-full w-full image rounded-lg flex lg:object-cover lg:object-left md:object-cover md:object-center object-center object-cover' />
       </div>
       <div class='flex justify-center text-3xl font-semibold'>
         Terminé
