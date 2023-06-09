9a3153bed44ca22110a8d99ff18ef3576ce8f9d6
della3a <manelky@gmail.com>
2023-06-09 17:56:06 +0200

    AVTR-TEST: image resolution fix

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class='overflow-hidden'>
     <div :class='getBackgroundClasses()' class='relative text-violet-700' >
       <HeaderBar class='absolute top-0' />
-      <NuxtPage class='h-screen w-screen ' />
+      <NuxtPage class='h-screen w-screen overflow-hidden' />
     </div>
   </div>
 </template>
